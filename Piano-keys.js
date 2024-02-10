d1a3ef22a8b31cbf950d7d9c9919712f61848ce9
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-02-09 18:29:13 -0800

    Update Piano-keys.js

--- Piano-keys.js
+++ Piano-keys.js
@@ -45,11 +45,15 @@ here are a few more examples:
 
 // Solution
 
-function blackOrWhiteKey(key) {
-  key = key%88;
+function blackOrWhiteKey(pianoKey) {
+  pianoKey = key%88;
   let black  = [2, 4, 7, 9, 11];
   if (key > 3) key = (key-3)%12;
   return (black.includes(key)) ? 'black' : 'white';
 }
 
 // or
+
+function blackOrWhiteKey(keyPressCount) {    
+  return ['black','white'] [ '101101011010'[((keyPressCount-1)%88)%12] ]
+}
